9a7637cd332ade48784abece4f3667d2c88dcdc4
GiuseppeRiccardoPugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-05-22 13:43:33 +0200

    Creazione della mia classe astratta Animale con all'interno 2 metodi astratti mangia() & verso() mentre un metodo locale dormi()

---
+++ src/org/exercise/Animale.java
@@ -0,0 +1,13 @@
+package org.exercise;
+
+public abstract class Animale {
+
+    public void dormi(){
+        System.out.println("Zzz");
+    }
+
+    //Abstract method
+    public abstract void verso();
+    public abstract void mangia();
+
+}
